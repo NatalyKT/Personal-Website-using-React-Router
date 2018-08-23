f47ec1b4a68028619c8fd97af5af86e13bc1a306
Natalia <36547456+NatalyKT@users.noreply.github.com>
2018-08-23 16:40:16 +0400

    import the NavBar component into App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import NavBar from "./NavBar.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <h1>Hello World</h1>
+        <NavBar />
       </div>
     </BrowserRouter>
   );

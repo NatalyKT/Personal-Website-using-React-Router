25d7dd621e144f5db8b913c6d81eafd4ae599dce
Natalia <36547456+NatalyKT@users.noreply.github.com>
2018-08-23 16:36:15 +0400

    Added description of the NavTab
    Added description of the principle of the work file

--- src/components/NavTab.jsx
+++ src/components/NavTab.jsx
@@ -1,3 +1,8 @@
+//The NavTab component is just a NavLink component with
+//some style and an activeStyle attribute that makes it turn
+//green and bold when it is active. Its to attribute is passed
+//down as a props and will determine where it will link to.
+//Its label is also passed down as a props.
 import React from "react";
 import { NavLink } from "react-router-dom";
 
